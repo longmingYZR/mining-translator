be83b1ea58c1afb7b45fb2cc9533b24519c25c69
longmingyzr <longming.yzr@gmail.com>
2026-05-29 14:35:31 +0800

    fix: use full Python path in bat launcher

--- 启动翻译工具.bat
+++ 启动翻译工具.bat
@@ -2,6 +2,8 @@
 chcp 65001 >nul
 title 矿业翻译工具 Mining Translator
 
+set "PYTHON_EXE=C:\Users\28307\AppData\Local\Python\pythoncore-3.14-64\python.exe"
+
 cd /d "%~dp0"
 
 echo 正在启动矿业翻译工具...
@@ -9,5 +11,5 @@ echo 浏览器将自动打开 http://127.0.0.1:7860
 echo 关闭此窗口即可停止服务
 echo.
 
-python -m mining_translator.gui
+"%PYTHON_EXE%" -m mining_translator.gui
 pause
